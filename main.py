6ebff4cd6a4c5a94b57c246b87d42d90936d70f0
elvitaly12 <vitaly932506@gmail.com>
2021-12-30 18:34:47 +0200

    db functions work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sqlalchemy.dialects.postgresql import psycopg2
 
 import config
+
 from DatabaseLogic import createDB
 from bot_logic import runbot
 from server_logic import app
@@ -9,7 +10,7 @@ from passwordfile import password
 
 import threading
 import sqlalchemy
-from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import create_engine, ForeignKey, false
 from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, Boolean
@@ -34,92 +35,36 @@ class TelegramThread(threading.Thread):
 #         createDB()
 
 
-class Users(db.Model):
-    username = db.Column(db.String(30), primary_key=True)
-    active = db.Column(db.Boolean, nullable=False)
-    chat_id = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, username, active, chat_id):
-        self.username = username
-        self.active = active
-        self.chat_id = chat_id
-
-    def __repr__(self):
-        # return '<User %r>' % self.username
-        return "<Users(username='{}', active={}, chat_id={})>" \
-            .format(self.username, self.active, self.chat_id)
 
 
-class Admins(db.Model):
-    username = db.Column(db.String(30), primary_key=True)
-    password = db.Column(db.String(20), nullable=False)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return "<Admins(username='{}', password='{}')>" \
-            .format(self.username, self.password)
 
 
-class Polls(db.Model):
-    poll_id = db.Column(db.Integer, primary_key=True)
-
-    def __init__(self, poll_id):
-        self.poll_id = poll_id
-
-    def __repr__(self):
-        return "<Polls(poll_id={})>" \
-            .format(self.poll_id)
+def Check_User_Registered(user_name_inserted,db):
+    exists = db.session.query(Users).filter_by(username=user_name_inserted).first() is not None
+    print(exists)
+    if exists:
+        print("USER  ALREADY REGISTERED")
 
 
-class Questions(db.Model):
-    question_id = db.Column(db.Integer, primary_key=True)
-    poll_id = db.Column(db.Integer, ForeignKey('Polls.poll_id'), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
 
-    def __init__(self, question_id, poll_id, description):
-        self.question_id = question_id
-        self.poll_id = poll_id
-        self.description = description
-
-    def __repr__(self):
-        return "<Questions(question_id={}, poll_id={}, description='{}')>" \
-            .format(self.question_id, self.poll_id, self.description)
+def Register_NewUser(username, chat_id, active, db):
+    newuser = Users(username, chat_id, active)
+    db.session.add(newuser)
+    db.session.commit()
 
 
-class Answers(db.Model):
-    answer_id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer, ForeignKey('Questions.question_id'), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
-
-    def __init__(self, answer_id, question_id, description):
-        self.answer_id = answer_id
-        self.question_id = question_id
-        self.description = description
-
-    def __repr__(self):
-        return "<Answers(answer_id={}, question_id={}, description='{}')>" \
-            .format(self.answer_id, self.question_id, self.description)
-
-
-class PollsAnswers(db.Model):
-    username = db.Column(db.String(30), ForeignKey('Users.username'), primary_key=True)
-    poll_id = db.Column(db.Integer, ForeignKey('Polls.poll_id'), primary_key=True)
-    question_id = db.Column(db.Integer, ForeignKey('Questions.question_id'), primary_key=True)
-    answer_id = db.Column(db.Integer, ForeignKey('Answers.answer_id'), primary_key=True)
-
-    def __init__(self, username, poll_id, question_id, answer_id):
-        self.username = username
-        self.poll_id = poll_id
-        self.question_id = question_id
-        self.answer_id = answer_id
-
-    def __repr__(self):
-        return "<PollsAnswers(username='{}', poll_id={}, question_id={}, answer_id={})>" \
-            .format(self.username, self.poll_id, self.question_id, self.answer_id)
-
+def User_UnRegister(user_name_inserted, chat_id, db):
+    name = db.session.query(Users).filter_by(chat_id=chat_id).first().username
+    print(name)
+    if name is None:
+        print("user with name + chat id does not exists")
+    elif name!= user_name_inserted:
+        print("access denied :  cant unregistered other users")
+    else:
+        db.session.query(Users) \
+            .filter(Users.username==user_name_inserted) \
+            .update({Users.active:False})
+        db.session.commit()
 
 if __name__ == "__main__":
     # app = Flask(__name__)
@@ -130,18 +75,101 @@ if __name__ == "__main__":
 
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:'+password+'@localhost:5433/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:'+password+'@localhost:5434/postgres'
+    db = SQLAlchemy(app)
+    class Users(db.Model):
+        username = db.Column(db.String(30), primary_key=True)
+        chat_id = db.Column(db.Integer, nullable=False)
+        active = db.Column(db.Boolean, nullable=False)
+
+        def __init__(self, username, chat_id, active):
+            self.username = username
+            self.chat_id = chat_id
+            self.active = active
+
+        def __repr__(self):
+            # return '<User %r>' % self.username
+            return "<Users(username='{}', chat_id={}, active={})>" \
+                .format(self.username, self.chat_id, self.active)
 
 
-    db.create_all()
-    db.session.commit()
-    admin = Users('admin', 'admin@example.com')
-    guest = Users('guest', 'guest@example.com')
-    db.session.add(admin)
-    db.session.add(guest)
-    db.session.commit()
-    users = Users.query.all()
-    print(users)
+    class Admins(db.Model):
+        username = db.Column(db.String(30), primary_key=True)
+        password = db.Column(db.String(20), nullable=False)
+
+        def __init__(self, username, password):
+            self.username = username
+            self.password = password
+
+        def __repr__(self):
+            return "<Admins(username='{}', password='{}')>" \
+                .format(self.username, self.password)
+
+
+    class Polls(db.Model):
+        poll_id = db.Column(db.Integer, primary_key=True)
+
+        def __init__(self, poll_id):
+            self.poll_id = poll_id
+
+        def __repr__(self):
+            return "<Polls(poll_id={})>" \
+                .format(self.poll_id)
+
+
+    class Questions(db.Model):
+        question_id = db.Column(db.Integer, primary_key=True)
+        poll_id = db.Column(db.Integer, ForeignKey('polls.poll_id'), nullable=False)
+        description = db.Column(db.String(300), nullable=False)
+
+        def __init__(self, question_id, poll_id, description):
+            self.question_id = question_id
+            self.poll_id = poll_id
+            self.description = description
+
+        def __repr__(self):
+            return "<Questions(question_id={}, poll_id={}, description='{}')>" \
+                .format(self.question_id, self.poll_id, self.description)
+
+
+    class Answers(db.Model):
+        answer_id = db.Column(db.Integer, primary_key=True)
+        question_id = db.Column(db.Integer, ForeignKey('questions.question_id'), nullable=False)
+        description = db.Column(db.String(300), nullable=False)
+
+        def __init__(self, answer_id, question_id, description):
+            self.answer_id = answer_id
+            self.question_id = question_id
+            self.description = description
+
+        def __repr__(self):
+            return "<Answers(answer_id={}, question_id={}, description='{}')>" \
+                .format(self.answer_id, self.question_id, self.description)
+
+
+    class PollsAnswers(db.Model):
+        username = db.Column(db.String(30), ForeignKey('users.username'), primary_key=True)
+        poll_id = db.Column(db.Integer, ForeignKey('polls.poll_id'), primary_key=True)
+        question_id = db.Column(db.Integer, ForeignKey('questions.question_id'), primary_key=True)
+        answer_id = db.Column(db.Integer, ForeignKey('answers.answer_id'), primary_key=True)
+
+        def __init__(self, username, poll_id, question_id, answer_id):
+            self.username = username
+            self.poll_id = poll_id
+            self.question_id = question_id
+            self.answer_id = answer_id
+
+        def __repr__(self):
+            return "<PollsAnswers(username='{}', poll_id={}, question_id={}, answer_id={})>" \
+                .format(self.username, self.poll_id, self.question_id, self.answer_id)
+
+
+
+
+
+
+
+
 
 
 
